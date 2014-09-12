381498745a8974abd08dae8c704c0c59868e4229
naleite <naleite.nuaa@gmail.com>
2014-09-12 21:23:25 +0200

    Initial Commit

---
+++ GameTest1Tests/GameTest1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GameTest1Tests.swift
+//  GameTest1Tests
+//
+//  Created by Leite.NA on 14-9-12.
+//  Copyright (c) 2014年 Leite.NA. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GameTest1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
